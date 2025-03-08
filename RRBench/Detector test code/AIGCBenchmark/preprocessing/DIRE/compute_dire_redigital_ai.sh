93d8ae2c12c5108eb99686d9ba34e9cb18127999
ChunXiaostudy <120032663+ChunXiaostudy@users.noreply.github.com>
2025-03-08 16:03:53 +0800

    Update compute_dire_redigital_ai.sh

--- RRBench/Detector test code/AIGCBenchmark/preprocessing/DIRE/compute_dire_redigital_ai.sh
+++ RRBench/Detector test code/AIGCBenchmark/preprocessing/DIRE/compute_dire_redigital_ai.sh
@@ -3,7 +3,7 @@
 export CUDA_VISIBLE_DEVICES=2
 export NCCL_P2P_DISABLE=1
 
-MODEL_PATH="/home/zhuyao123/lcx/AIGCDetectBenchmark-main/weights/preprocessing/lsun_bedroom.pt"
+MODEL_PATH="/home/AIGCDetectBenchmark-main/weights/preprocessing/lsun_bedroom.pt"
 MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond False --diffusion_steps 1000 --dropout 0.1 --image_size 256 --learn_sigma True --noise_schedule linear --num_channels 256 --num_head_channels 64 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
 
 # Input and output directories
