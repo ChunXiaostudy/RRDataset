871f584ca27ff93b3360ceabb7cf7d07d3d9611b
ChunXiaostudy <120032663+ChunXiaostudy@users.noreply.github.com>
2025-03-08 16:05:52 +0800

    Update compute_dire_transfer_real.sh

--- RRBench/Detector test code/AIGCBenchmark/preprocessing/DIRE/compute_dire_transfer_real.sh
+++ RRBench/Detector test code/AIGCBenchmark/preprocessing/DIRE/compute_dire_transfer_real.sh
@@ -3,12 +3,12 @@
 export CUDA_VISIBLE_DEVICES=5
 export NCCL_P2P_DISABLE=1
 
-MODEL_PATH="/home/zhuyao123/lcx/AIGCDetectBenchmark-main/weights/preprocessing/lsun_bedroom.pt"
+MODEL_PATH="/home/AIGCDetectBenchmark-main/weights/preprocessing/lsun_bedroom.pt"
 MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond False --diffusion_steps 1000 --dropout 0.1 --image_size 256 --learn_sigma True --noise_schedule linear --num_channels 256 --num_head_channels 64 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
 
 # Input and output directories
-INPUT_DIR="/data/lcx/RRDataset_final/transfer_real_images"
-OUTPUT_DIR="/data/lcx/DIRE_IMAGE/transfer_real_images"
+INPUT_DIR="/data/RRDataset_final/transfer_real_images"
+OUTPUT_DIR="/data/DIRE_IMAGE/transfer_real_images"
 
 # Calculate number of images
 num_images=$(find "${INPUT_DIR}" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.JPEG" -o -iname "*.PNG" \) | wc -l)
@@ -16,4 +16,4 @@ num_images=$(find "${INPUT_DIR}" -type f \( -iname "*.jpg" -o -iname "*.png" -o 
 SAVE_FLAGS="--images_dir $INPUT_DIR --recons_dir $OUTPUT_DIR --dire_dir $OUTPUT_DIR"
 SAMPLE_FLAGS="--batch_size 8 --num_samples $num_images --timestep_respacing ddim20 --use_ddim True"
 
-mpiexec -n 1 python /home/zhuyao123/lcx/AIGCDetectBenchmark-main/preprocessing/DIRE/compute_dire.py --model_path $MODEL_PATH $MODEL_FLAGS $SAVE_FLAGS $SAMPLE_FLAGS --has_subfolder False --has_subclasses False 
+mpiexec -n 1 python /home/AIGCDetectBenchmark-main/preprocessing/DIRE/compute_dire.py --model_path $MODEL_PATH $MODEL_FLAGS $SAVE_FLAGS $SAMPLE_FLAGS --has_subfolder False --has_subclasses False 
